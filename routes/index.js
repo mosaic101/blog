2b588605e32ef0e3583dcda1833cebf887db983b
mosaic101 <mosaic101@foxmail.com>
2016-08-19 00:10:45 +0800

    add /change route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var router = require('koa-router')();
-
+var fs = require('fs');
+var markdown = require('markdown').markdown;
 router.get('/', async (ctx, next) => {
    //ctx.state = {
    //  title: 'koa2 title'
@@ -9,6 +10,18 @@ router.get('/', async (ctx, next) => {
   await ctx.render('index', {
     title:'吴建金的博客',
     name:'world'
+  });
+});
+
+//查看更新log
+router.get('/change', (ctx, next) => {
+  fs.readFile("./CHANGELOG.md", "utf8", function (error, data) {
+    if (error) {
+      console.log(error);
+      //return next(error);
+    } else {
+      ctx.body = markdown.toHTML(data);
+    }
   });
 });
 
