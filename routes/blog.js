818c3a0c628fa40cf7c0ba35416189e4fd881eb0
mosaic101 <mosaic101@foxmail.com>
2016-11-17 23:30:52 +0800

    delete extra code

--- routes/blog.js
+++ routes/blog.js
@@ -27,6 +27,31 @@ router.get('/', async (ctx, next) => {
     }
 });
 
+//单个详情
+router.get('/one/:id', async (ctx,next) => {
+    let id = ctx.params.id;
+    if (!id || !_.isString(id)) {
+        ctx.body = {
+            tag:'error',
+            status:-1,
+            message:'参数错误!'
+        };
+    }
+    try {
+        var blog = await blogService.one(id);
+        await ctx.render('blog/detail', {
+            title: blog.title,
+            blog: blog
+        });
+    }catch (err) {
+        ctx.body = {
+            tag:'error',
+            status:err.status,
+            message:err.message
+        };
+    }
+});
+
 //添加
 router.post('/add', async (ctx,next) => {
     var blog = {
@@ -56,73 +81,7 @@ router.post('/add', async (ctx,next) => {
     }
 });
 
-//首页推荐
-router.get('/commend',async (ctx,next) => {
-    let offset = ctx.params.offset || 0;
-    let limit = ctx.params.limit || 6;
-    let where = {state: 'published'};
-    try {
-        var result = await blogService.commend(where, offset, limit);
-        ctx.body = {
-            tag:'success',
-            status:1,
-            message:'查询成功!',
-            data:result
-        };
-    }catch (err) {
-        ctx.body = {
-            tag:'error',
-            status:err.status,
-            message:err.message
-        };
-    }
-});
 
-//列表
-router.get('/list', async (ctx,next) => {
-    let offset = ctx.params.offset || 0;
-    let limit = ctx.params.limit || 11;
-    let where = {state: 'published'};
-    try {
-        var result = await blogService.list(where, offset, limit);
-        ctx.body = {
-            tag:'success',
-            status:1,
-            message:'查询成功!',
-            data:result
-        };
-    }catch (err) {
-        ctx.body = {
-            tag:'error',
-            status:err.status,
-            message:err.message
-        };
-    }
-});
 
-//单个详情
-router.get('/one/:id', async (ctx,next) => {
-    let id = ctx.params.id;
-    if (!id || !_.isString(id)) {
-        ctx.body = {
-            tag:'error',
-            status:-1,
-            message:'参数错误!'
-        };
-    }
-    try {
-        var blog = await blogService.one(id);
-        await ctx.render('blog/detail', {
-            title: blog.title,
-            blog: blog
-        });
-    }catch (err) {
-        ctx.body = {
-            tag:'error',
-            status:err.status,
-            message:err.message
-        };
-    }
-});
 
 export default router;
