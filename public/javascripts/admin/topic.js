7a59813f959a01ad22d986d1d8fd54486b0252b7
mosaic101 <mosaic101@foxmail.com>
2016-11-30 18:06:08 +0800

    modify simplemdeMDE

--- public/javascripts/admin/topic.js
+++ public/javascripts/admin/topic.js
@@ -1,3 +1,9 @@
 /**
  * Created by wujj on 2016/11/29.
  */
+$(function () {
+    //提交按鈕
+    $('#publishTopic').on('click',function () {
+        console.log(simplemde.value());
+    })
+});
---
+++ public/libs/editor/initEditor.js
@@ -0,0 +1,58 @@
+/**
+ * 初始化editor
+ * Created by wujj on 2016/11/30.
+ */
+var simplemde = new SimpleMDE({
+    autofocus: true, //光标聚焦
+    autosave: {
+        enabled: true,
+        uniqueId: "postTopic",
+        delay: 5000,
+    },
+    element: document.getElementById("editor"),
+    forceSync: true,
+    hideIcons: ["guide", "heading"],
+    indentWithTabs: false,
+    insertTexts: {
+        horizontalRule: ["", "\n\n-----\n\n"],
+        image: ["![](http://", ")"],
+        link: ["[", "](http://)"],
+        table: ["", "\n\n| Column 1 | Column 2 | Column 3 |\n| -------- | -------- | -------- |\n| Text     | Text      | Text     |\n\n"],
+    },
+    lineWrapping: true, //自动换行
+    parsingConfig: {
+        allowAtxHeaderWithoutSpace: true,
+        strikethrough: false,
+        underscoresBreakWords: true,
+    },
+    placeholder: "Type here...",
+   // previewRender: function(plainText, preview) { // Async method
+   //     setTimeout(function(){
+   //         preview.innerHTML = customMarkdownParser(plainText);
+   //     }, 250);
+   //
+   //     return "Loading...";
+   // },
+    promptURLs: true,
+    renderingConfig: {
+        singleLineBreaks: false,
+        codeSyntaxHighlighting: true,
+    },
+    shortcuts: {
+        drawTable: "Cmd-Alt-T"
+    },
+    showIcons: ["code", "table"],
+    spellChecker: false,
+    status: ["autosave", "lines", "words", "cursor", {
+        className: "keystrokes",
+        defaultValue: function(el) {
+            this.keystrokes = 0;
+            el.innerHTML = "0 Keystrokes";
+        },
+        onUpdate: function(el) {
+            el.innerHTML = ++this.keystrokes + " Keystrokes";
+        }
+    }],
+    styleSelectedText: false,
+    tabSize: 4
+});
